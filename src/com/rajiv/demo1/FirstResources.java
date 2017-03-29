8393e1efc6b30dfc7cd78cbeee73b09ce1eba405
Rajiv <ranjan.rajiv2010@gmail.com>
2017-03-29 19:18:01 +0530

    first demo

---
+++ src/com/rajiv/demo1/FirstResources.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.rajiv.demo1;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author Raji
+ *
+ */
+@Path("/first")//url for the resource
+public class FirstResources {
+
+	@GET//this is a get request
+	@Produces("text/html")//mime type to be produce by this resource
+	public String getMessage(){
+		return"<html><body><h2>Hey this is my first rest project </body></body></html>";
+	}
+	
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	@Path("/second/{name}")
+	public String getparamMessage(@PathParam("name") String name){
+		return "first name is "+name;
+	}
+	
+}
